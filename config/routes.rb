cc08d658d0c35840dea7edc64ff851a536a48ac6
Okamoto Aya <igm_pman@igapimannoMacBook-Air.local>
2022-06-24 19:18:51 +0900

    Add: 最低限の新規作成機能、ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
   root 'static_pages#top'
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]  
 end
